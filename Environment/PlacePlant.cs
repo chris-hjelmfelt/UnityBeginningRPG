0a6ab6e1a67c485d8af2658a1bc107b646ad9356
Chris Hjelmfelt <christine.hjelmfelt@gmail.com>
2022-04-09 20:16:35 -0600

    dynamically place plants

---
+++ Environment/PlacePlant.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacePlant : MonoBehaviour
+{  
+	  public GameObject plant;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {      
+        Vector3 Loc1 = new Vector3 (316, 100, 346);  
+        Vector3 Loc2 = new Vector3 (319, 100, 348); 
+        Vector3 Loc3 = new Vector3 (312, 100, 343); 
+        Vector3 Loc4 = new Vector3 (314, 100, 346);
+        Vector3 Loc5 = new Vector3 (316, 100, 342);
+        Vector3 Loc6 = new Vector3 (318, 100, 345);
+        Vector3[] locations = new Vector3[] { Loc1, Loc2, Loc3, Loc4, Loc5, Loc6 };
+
+        for(int i=0; i < 3; i++) {          
+          int rnd = Random.Range(0,locations.Length);
+          var newPlant = Instantiate(
+              plant, 
+              new Vector3(locations[rnd].x, locations[rnd].y, locations[rnd].z), 
+              Quaternion.identity
+          );
+          newPlant.transform.localScale = new Vector3(4,4,4);
+          locations = removeAtIndex(locations, rnd);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     Vector3[] removeAtIndex(Vector3[] inputArray, int index){
+        Vector3[] outputArray = new Vector3[inputArray.Length-1];
+        for(int i=0; i < index; i++){
+            outputArray[i] = inputArray[i];
+        }        
+        
+        for(int writeLoc=index; writeLoc < outputArray.Length; writeLoc++){
+            outputArray[writeLoc] = inputArray[writeLoc+1];
+        }
+        return outputArray;
+    }
+}
--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-# UnityBeginningRPG
+# UnityBeginningRPG    
 
-A 3D Role Playing Game made using the Unity engine. It is currently just a project to learn and experiment. 
+A 3D Role Playing Game made using the Unity engine.   
 
 I originally started a Unity RPG project with a set of tutorials from Brackeys on YouTube:
 "How to make an RPG in Unity - Introduction"  https://www.youtube.com/watch?v=nu5nyrB9U_o&t=1s
-This is a continuation of that project. I am experimenting with terrain, 3D models, adding 
-features for crafting and building, and creating quests. This may become a fully playable game 
-that I release at some point or more likely it will provide the building blocks for a future RPG.
+This is a continuation of that project.     
 
-For now this repo contains only the C# scripts, and none of the other assets.
+I have been experimenting with terrain, adding models I created from Blender, adding features for crafting and building, 
+creating quests, dynamically placing items, and more.     
 
-I originally started the Brackey's tutorial March 2019.
+This provides the building blocks for future RPGs.    
 
